3c06ccec273f3e1b14b0b7aee93bb1fcd8fd57c9
gin4ever <65288202+gin4ever@users.noreply.github.com>
2021-08-11 23:23:45 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using eProject.Services;
+using eProject.Repository;
+using Microsoft.EntityFrameworkCore;
+namespace eProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string url = "server=DESKTOP-TEK2ARS\\SQLEXPRESS;database=stationerydb;uid=sa;pwd=123";
+            services.AddDbContext<StationeryContext>(options => options.UseSqlServer(url));
+            services.AddScoped<IUsersServices, UsersServices>();
+            services.AddScoped<IItemServices, ItemServices>();
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Item}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
